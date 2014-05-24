c3a549b0d960a15ac813576cb5079b4fd936a61c
Tomoki Imai <tomo832@gmail.com>
2014-05-24 21:31:11 +0900

    Add function parser(initial).

--- src/syntax/ast.hpp
+++ src/syntax/ast.hpp
@@ -1,6 +1,8 @@
 #ifndef AST_HPP_INCLUDED
 #define AST_HPP_INCLUDED
 
+#include "../misc.hpp"
+#include <iostream>
 #include <boost/variant.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
@@ -15,38 +17,119 @@ namespace shiranui{
             namespace qi = boost::spirit::qi;
             namespace ph = boost::phoenix;
 
+            // meta struct
             struct LocationInfo;
-            struct Identifier;
+            struct Identifier; // should not use.
+            struct SourceCode;
+
+            // immediate value.
+            struct Number;
+            struct String;
+            struct Function;
+            struct Variable;
+
+            // expression
+            struct FunctionCall;
+
+            // statements.
             struct VarDefinement;
             struct ConstDefinement;
-            struct SourceCode;
+
+
+
+            typedef boost::variant<Number,
+                                   String,
+                                   Function,
+                                   Variable,
+                                   FunctionCall> Expression;
 
             typedef boost::variant<VarDefinement,
                                    ConstDefinement> Statement;
+
             struct LocationInfo{
                 unsigned int line,column,length;
                 virtual std::ostream& serialize(std::ostream&) const = 0;
+                friend std::ostream& operator<<(std::ostream&,const LocationInfo&);
             };
+
+            // metaelement.
             struct Identifier : LocationInfo{
                 std::string name;
                 std::ostream& serialize(std::ostream &os) const{
                     return os << name;
                 }
             };
-            struct VarDefinement : LocationInfo{
-                Identifier id;
+
+            // immediate values.
+            struct Variable : LocationInfo{
+                Identifier value;
+                std::ostream& serialize(std::ostream &os) const{
+                    return os << value;
+                }
+            };
+            struct Number : LocationInfo{
                 int value;
                 std::ostream& serialize(std::ostream &os) const{
-                    return os << "mut " << id.name << "-> " << value;
+                    return os << value;
+                }
+            };
+            struct String : LocationInfo{
+                std::string value;
+                std::ostream& serialize(std::ostream &os) const{
+                    return os << value;
+                }
+            };
+            struct Function : LocationInfo{
+                std::vector<Identifier> parameters;
+                std::vector<Statement> body;
+                std::ostream& serialize(std::ostream &os) const{
+                    os << "\\(";
+                    for(size_t i=0;i<parameters.size();i++){
+                        os << parameters[i];
+                        if(i != parameters.size()-1){
+                            os << ",";
+                        }
+                    }
+                    os << "){";
+                    for(size_t i=0;i<body.size();i++){
+                        // os << body[i] << std::endl;
+                        os << "hoge" << std::endl;
+                    }
+                    return os << "}";
+                }
+            };
+
+            // expression.
+            struct FunctionCall : LocationInfo{
+                Identifier function_name;
+                std::vector<Expression> arguments;
+                std::ostream& serialize(std::ostream &os) const{
+                    os << function_name << "(";
+                    for(size_t i=0;i<arguments.size();i++){
+                        os << arguments[i];
+                        if(i != arguments.size()-1){
+                            os << ",";
+                        }
+                    }
+                    return os << ")";
+                }
+            };
+            // statement.
+            struct VarDefinement : LocationInfo{
+                Identifier id;
+                Expression value;
+                std::ostream& serialize(std::ostream &os) const{
+                    return os << "mut " << id << "-> " << value;
                 }
             };
             struct ConstDefinement : LocationInfo{
                 Identifier id;
-                int value;
+                Expression value;
                 std::ostream& serialize(std::ostream &os) const{
-                    return os << "let " << id.name << "-> " << value;
+                    return os << "let " << id << "-> " << value;
                 }
             };
+
             struct SourceCode : LocationInfo{
                 std::vector<Statement> statements;
                 std::ostream& serialize(std::ostream &os) const{
@@ -56,14 +139,37 @@ namespace shiranui{
         }
      }
 }
+
+// meta structs
 BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::Identifier,
                           (std::string,name));
+// immeidate value (also,it is expression).
+BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::Number,
+                          (int,value));
+BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::String,
+                          (std::string,value));
+BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::Variable,
+                          (shiranui::syntax::ast::Identifier,value));
+// BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::Function,
+//                           (std::vector<shiranui::syntax::ast::Identifier>,parameters)
+//                           (std::vector<shiranui::syntax::ast::Statement>,body));
+BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::Function,
+                          (std::vector<shiranui::syntax::ast::Identifier>,parameters)
+                          (std::vector<shiranui::syntax::ast::Statement>,body));
+                          // );
+
+// expression
+BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::FunctionCall,
+                          (shiranui::syntax::ast::Identifier,function_name)
+                          (std::vector<shiranui::syntax::ast::Expression>,arguments));
+// statements.
 BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::VarDefinement,
                           (shiranui::syntax::ast::Identifier,id)
-                          (int,value));
+                          (shiranui::syntax::ast::Expression,value));
 BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::ConstDefinement,
                           (shiranui::syntax::ast::Identifier,id)
-                          (int,value));
+                          (shiranui::syntax::ast::Expression,value));
+
 BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::SourceCode,
                           (std::vector<shiranui::syntax::ast::Statement>,statements)
                           );
@@ -72,10 +178,20 @@ BOOST_FUSION_ADAPT_STRUCT(shiranui::syntax::ast::SourceCode,
 namespace shiranui{
     namespace syntax{
         namespace ast{
+            // struct expression_serializer : boost::static_visitor<std::ostream&>{
+            //     std::ostream& operator()(const LocationInfo& s,std::ostream& os) const{
+            //         return s.serialize(os);
+            //     }
+            // };
             std::ostream& operator<<(std::ostream& os,
                                      const shiranui::syntax::ast::LocationInfo& s){
                 return s.serialize(os);
             }
+            // std::ostream& operator<<(std::ostream& os,
+            //                          const shiranui::syntax::ast::Expression& e){
+            //     // return e.serialize(os);
+            //     return boost::apply_visitor(expression_serializer(),e,os);
+            // }
         }
     }
 }
--- src/syntax/parser.hpp
+++ src/syntax/parser.hpp
@@ -49,7 +49,6 @@ namespace shiranui{
             }
             template<typename T1,typename T2,typename T3,typename T4>
             result_type operator()(T1 b,T2 e,T3 where,const T4& what) const{
-                std::string actual_remain = std::string(b,e);
                 std::cerr << "Error expecting " << what << " in line " << get_line(where) << ":" << std::endl
                           << std::string(b,e) << std::endl
                           << std::setw(std::distance(b,where)) << "^---- here" << std::endl;
@@ -91,17 +90,32 @@ namespace shiranui{
                 keyword_arrow = lit("->");
                 semicolon = lit(";");
                 one_equal = lit("=");
+                keyword_lambda = lit("\\");
 
-                identifier %= (char_("_") | alpha) >> *(alnum | char_("_"));
+                identifier = as_string[(alpha | char_('_')) >> *(alnum | char_('_'))] ;
+                number = int_;
+                string = lexeme[lit("\"") > *(char_ - "\"") > lit("\"")];
+                variable = identifier;
+
+                function = keyword_lambda >> '(' >> (identifier % ',') >> ')'
+                                          >> '{' >> *statement >> '}';
+
+                function_call = identifier >> lit("(") >> (expression % ",") >> lit(")");
+
                 // change int_ to something to object. change semicolon to eol?
                 // DONOT USE > and >> together.
-                const_definement = keyword_let > identifier > one_equal > int_ > semicolon;
-                var_definement   = keyword_mut > identifier > one_equal > int_ > semicolon;
-                // what qi::eps for?
+                const_definement = keyword_let > identifier > one_equal > expression > semicolon;
+                var_definement   = keyword_mut > identifier > one_equal > expression > semicolon;
 
-
-                source_code = qi::eps >> *(statement);
                 statement = (const_definement | var_definement);
+                expression = (function_call |
+                              number |
+                              string |
+                              function |
+                              variable
+                              );
+                // what qi::eps for?
+                source_code = qi::eps >> *(statement);
 
                 on_error<fail>(var_definement, handler(_1, _2, _3, _4));
                 on_error<fail>(const_definement, handler(_1, _2, _3, _4));
@@ -110,9 +124,29 @@ namespace shiranui{
                 auto set_location_info = annotate(_val,_1,_3);
 
                 on_success(identifier,set_location_info);
+                on_success(number,set_location_info);
+                on_success(string,set_location_info);
+                on_success(function,set_location_info);
+
+                on_success(function_call,set_location_info);
                 on_success(var_definement,set_location_info);
                 on_success(const_definement,set_location_info);
+
+                // cause error.
+                // on_success(expression,set_location_info);
+                // on_success(statement,set_location_info);
                 on_success(source_code,set_location_info);
+
+                identifier.name("identifier");
+                number.name("number");
+                string.name("string");
+                variable.name("variable");
+                function.name("function");
+                function_call.name("function_call");
+                var_definement.name("mut_defiment");
+                const_definement.name("const_defiment");
+                expression.name("expression");
+                statement.name("statement");
 
                 BOOST_SPIRIT_DEBUG_NODES((keyword_let)(keyword_mut)(keyword_arrow)(source_code)
                                          (semicolon)(openbrace)(closebrace)(statement)
@@ -121,16 +155,29 @@ namespace shiranui{
             ph::function<error_handler_f> handler;
             ph::function<annotation_f<Iterator>> annotate;
 
-            qi::rule<Iterator,ast::SourceCode(),Skipper> source_code;
+            // meta
             qi::rule<Iterator,ast::Identifier()> identifier;
+            // immidiate
+            qi::rule<Iterator,ast::Number()> number;
+            qi::rule<Iterator,ast::String()> string;
+            qi::rule<Iterator,ast::Variable()> variable;
+            qi::rule<Iterator,ast::Function(),Skipper> function;
+
+            // expression
+            qi::rule<Iterator,ast::FunctionCall(),Skipper> function_call;
+
+            // statements
             qi::rule<Iterator,ast::VarDefinement(),Skipper> var_definement;
             qi::rule<Iterator,ast::ConstDefinement(),Skipper> const_definement;
 
+            qi::rule<Iterator,ast::Expression(),Skipper> expression;
             qi::rule<Iterator,ast::Statement(),Skipper> statement;
+            qi::rule<Iterator,ast::SourceCode(),Skipper> source_code;
 
             qi::rule<Iterator> keyword_let;
             qi::rule<Iterator> keyword_mut;
             qi::rule<Iterator> keyword_arrow;
+            qi::rule<Iterator> keyword_lambda;
 
             qi::rule<Iterator> one_equal;
             qi::rule<Iterator> semicolon;
