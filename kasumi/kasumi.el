216f2b167e4173d91d4a2e195743bd6994986edd
Tomoki Imai <tomo832@gmail.com>
2014-06-09 22:11:58 +0900

    Add mini syntax highlight.

--- kasumi/kasumi.el
+++ kasumi/kasumi.el
@@ -13,15 +13,31 @@
     ))
 
 ;; getline needs newline("\n")
-(defun buffer-string-no-properties
+(defun buffer-string-no-properties ()
   (buffer-substring-no-properties (point-min) (point-max)))
+
+
+(defconst kasumi-font-lock-keywords
+  (list
+   (cons "\\<\\(else\\|if\\|let\\|mut\\|then\\|return\\)\\>" 'font-lock-keyword-face)
+   (cons "\\(\\\\\\)" 'font-lock-constant-face)
+   (cons "\\<\\(and\\|or\\)\\>" 'font-lock-builtin-face)
+   (cons "not\\>" 'font-lock-builtin-face)
+   ;; (cons "let[ \t\n]*[a-zA-Z]+[0-9]*" 'font-lock-variable-name-face)
+   (cons "\\<[\\-+]*[0-9]*\\.?[0-9]+\\>" font-lock-constant-face)
+   ))
 
 ;; http://www.emacswiki.org/emacs/ModeTutorial
 (defun kasumi-mode ()
   "Major mode for editing Shiranui files"
   (interactive)
   (kill-all-local-variables)
+  (make-local-variable 'shiranui-process)
+  (set (make-local-variable 'font-lock-defaults) '(kasumi-font-lock-keywords))
   ;; (set-syntax-table kasumi-mode-syntax-table)
-  (set-local-map kasumi-mode-map))
+  (use-local-map kasumi-mode-map)
+  (setq major-mode 'kasumi-mode)
+  (setq mode-name "Kasumi")
+  (run-hooks 'kasumi-mode-hook))
 
 (provide 'kasumi-mode)
