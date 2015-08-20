de41e1108e6c0ee1d0da3a2067be84ca45ef2aa0
Tomoki Imai <tomo832@gmail.com>
2015-08-20 21:52:50 +0900

    remove unneeded info

--- kasumi/kasumi.el
+++ kasumi/kasumi.el
@@ -360,7 +360,7 @@
          )
     ;; (kasumi-debug-print (format "(%d,%d) = %s" start end value))))
     (progn
-      (kasumi-debug-print (format "%s at [%d,%d] = %s"
+      (kasumi-debug-print (format "%s [%d,%d] = %s"
                                   (concat (filter (lambda (c) (not (= c ?\n))) (buffer-substring-no-properties start end)))
                                   start end value) t)
       (kasumi-put-explore start end)
